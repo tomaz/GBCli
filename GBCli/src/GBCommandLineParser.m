tomaz/GBCli

commit 7fdf6d5307f3e375f967b6bbba8f155168bdae5a

Support interlaced positional args and options
This commit adds the ability to parse interlaced arguments and options.
For example, the following:

    GBCli foo -o bar --print-settings

will now be parsed as:

    Arguments:
        foo

    Options:
        output         = bar
        print-settings

without needing to place all arguments after options.

GBCli/src/GBCommandLineParser.m
@@ -196,7 +196,6 @@ static NSString * const GBCommandLineNotAnOptionKey = @"not-an-option"; // this 
 		id value = nil;
 		NSString *input = [arguments objectAtIndex:index];
 		NSDictionary *data = [self optionDataForOption:input value:&value];
-		if (data == (id)GBCommandLineNotAnOptionKey) break; // no more options, only arguments left...
 		if (data == (id)GBCommandLineOptionGroupKey) {
 			// If this is group name, continue with next option...
 			handler(GBParseFlagOption, input, @YES, &stop);
@@ -204,7 +203,7 @@ static NSString * const GBCommandLineNotAnOptionKey = @"not-an-option"; // this 
 			continue;
 		}
 		
-		NSString *name = data[GBCommandLineLongOptionKey];
+		NSString *name = nil;
 		GBParseFlags flags = GBParseFlagOption;
 		
 		if (data == nil) {
@@ -217,7 +216,13 @@ static NSString * const GBCommandLineNotAnOptionKey = @"not-an-option"; // this 
 			name = input;
 			flags = GBParseFlagWrongGroup;
 			result = NO;
+		} else if (data == (id)GBCommandLineNotAnOptionKey) {
+			// If this is a non-option argument, notify observer.
+			value = input;
+			flags = GBParseFlagArgument;
 		} else {
+			name = data[GBCommandLineLongOptionKey];
+
 			// Prepare the value or notify about problem with it.
 			GBValueRequirements requirement = [data[GBCommandLineRequirementKey] unsignedIntegerValue];
 			switch (requirement) {
@@ -281,7 +286,7 @@ static NSString * const GBCommandLineNotAnOptionKey = @"not-an-option"; // this 
 		if (stop) return NO;
 		
 		// Remember parsed option and continue with next one.
-		if (value) [self.parsedOptions setObject:value forKey:name];
+		if (name && value) [self.parsedOptions setObject:value forKey:name];
 		index++;
 	}
 	

commit a059def8739afeb4482381976aff01f5cba48e4a

Fix group lookup error introduced in 7fdf6d5

GBCli/src/GBCommandLineParser.m
@@ -217,7 +217,8 @@ static NSString * const GBCommandLineEndOfOptionsKey = @"end-of-options"; // thi
 			name = input;
 			flags = GBParseFlagUnknownOption;
 			result = NO;
-		} else if (self.currentOptionsGroupOptions && ![self.currentOptionsGroupOptions containsObject:name]) {
+		} else if (self.currentOptionsGroupOptions
+				   && ![self.currentOptionsGroupOptions containsObject:data[GBCommandLineLongOptionKey]]) {
 			// If name of the option is not registered for the current group, notify observer.
 			name = input;
 			flags = GBParseFlagWrongGroup;